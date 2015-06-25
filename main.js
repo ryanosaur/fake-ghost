2f6ca08c7ca6c57118ff055ca1908282d37cd924
ryano <ryan.justin.taylor@gmail.com>
2015-06-24 21:32:53 -0700

    finished homework

---
+++ main.js
@@ -0,0 +1,44 @@
+var fs = require("fs"),
+  http = require("http"),
+  request = require('request'),
+  url = require('url'),
+  marked = require('marked'),
+  Firebase = require('firebase');
+
+http.createServer(responseHandler).listen(process.env.PORT || 8888);
+var ref = new Firebase("https://markdown-renderer.firebaseio.com/");
+var mdRef = ref.child('markdown');
+var htmlRef = ref.child('html');
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+function responseHandler(req, res) {
+  if (req.url.match("fav")) {
+    res.end("");
+    return;
+  }
+
+  var markdownRegex = /\/markdown\//;
+  if (req.url === "/") {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', 'utf8', function (err,data) {
+      res.end(data);
+    });
+  }else if(req.url.match(markdownRegex)){
+    var md2process = decodeURI(req.url.replace(markdownRegex, ''));
+    res.end(marked(md2process));
+  }
+  else{
+    res.end('<h1>this is not the page you\'re looking for O.o</h1>');
+  }
+}
+
